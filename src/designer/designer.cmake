0cb4c1fc43653ae13c081c3d14b987b69dd00174
Barbara Geller <barbara@copperspice.com>
2022-03-21 14:12:09 -0800

    Update CMake file

--- src/designer/designer.cmake
+++ src/designer/designer.cmake
@@ -1,23 +1,23 @@
 list(APPEND DESIGNER_INCLUDES
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_toolwindow.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_formwindow.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_workbench.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_settings.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_actions.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_server.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_appearanceoptions.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_components.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_toolwindow.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_formwindow.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_workbench.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_settings.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_actions.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_server.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_appearanceoptions.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_components.h
 )
 
 list(APPEND DESIGNER_SOURCES
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_toolwindow.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_formwindow.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_workbench.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_settings.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_server.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_actions.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_appearanceoptions.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/designer/qdesigner_components.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_toolwindow.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_formwindow.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_workbench.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_settings.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_server.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_actions.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_appearanceoptions.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/designer/designer_components.cpp
 )
