f947b8d9ac693d3acf186c48586d94c5d1a86411
Barbara Geller <barbara@copperspice.com>
2022-04-20 15:01:19 -0700

    Sort file names in build file

--- src/editor/editor.cmake
+++ src/editor/editor.cmake
@@ -7,33 +7,33 @@ list(APPEND DESIGNER_INCLUDES
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/buddy_editor/buddyeditor_plugin.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/buddy_editor/buddyeditor_tool.h
 
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/embedded_options_page.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_widgetstack.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_dnditem.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowcursor.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/widgetselection.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowmanager.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qlayoutwidget_propertysheet.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/layout_propertysheet.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/spacer_propertysheet.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/line_propertysheet.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_container.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_actionprovider.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_container.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_layoutdecoration.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/deviceprofiledialog.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/dpi_chooser.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/embedded_options_page.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor_optionspage.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_dnditem.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_widgetstack.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowcursor.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowmanager.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowsettings.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/itemview_propertysheet.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/layout_propertysheet.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/line_propertysheet.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/previewactiongroup.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qlayoutwidget_propertysheet.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qmainwindow_container.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qmdiarea_container.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qwizard_container.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_layoutdecoration.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/tool_widgeteditor.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor_optionspage.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowsettings.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/deviceprofiledialog.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/dpi_chooser.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/itemview_propertysheet.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/spacer_propertysheet.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/templateoptionspage.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/previewactiongroup.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/tool_widgeteditor.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/undostack.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/widgetselection.h
 
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/gradient_editor/color_button.h
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/gradient_editor/color_line.h
@@ -100,33 +100,33 @@ list(APPEND DESIGNER_SOURCES
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/buddy_editor/buddyeditor_tool.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/buddy_editor/buddyeditor_plugin.cpp
 
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_actionprovider.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_container.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_layoutdecoration.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/deviceprofiledialog.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/dpi_chooser.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/embedded_options_page.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_widgetstack.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_dnditem.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowcursor.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/widgetselection.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowmanager.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qlayoutwidget_propertysheet.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor_optionspage.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_dnditem.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindow_widgetstack.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowcursor.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowmanager.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowsettings.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/itemview_propertysheet.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/layout_propertysheet.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/spacer_propertysheet.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/line_propertysheet.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/previewactiongroup.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qlayoutwidget_propertysheet.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qmainwindow_container.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qmdiarea_container.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/qwizard_container.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_container.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_layoutdecoration.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/default_actionprovider.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/tool_widgeteditor.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formeditor_optionspage.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/formwindowsettings.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/deviceprofiledialog.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/dpi_chooser.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/previewactiongroup.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/itemview_propertysheet.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/spacer_propertysheet.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/templateoptionspage.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/tool_widgeteditor.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/undostack.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/editor/form_editor/widgetselection.cpp
 
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/gradient_editor/color_button.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/editor/gradient_editor/color_line.cpp
