c0e28d23d01fe5ff31c3ae44830dca523a3438b1
Barbara Geller <barbara@copperspice.com>
2025-01-09 12:26:11 -0800

    Update copyright

--- src/editor/form_editor/previewactiongroup.cpp
+++ src/editor/form_editor/previewactiongroup.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2021-2024 Barbara Geller
-* Copyright (c) 2021-2024 Ansel Sermersheim
+* Copyright (c) 2021-2025 Barbara Geller
+* Copyright (c) 2021-2025 Ansel Sermersheim
 *
 * Copyright (c) 2015 The Qt Company Ltd.
 *
